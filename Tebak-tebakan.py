4177c00d13084016849c2af6fe632d3f8a3e4a94
ragilalfarizi <alfariziragill@gmail.com>
2022-07-04 17:50:10 +0700

    Ada Typo

--- Tebak-tebakan.py
+++ Tebak-tebakan.py
@@ -3,4 +3,4 @@ print("Wow Oke")
 print("Cewek nih yang main!")
 print("ini tambahan line buat push")
 
-print("Aku ini adalah seorang kapiten mempunyai pedang panjang. Kalo berjalan hap-hap-hap."
+print("Aku ini adalah seorang kapiten mempunyai pedang panjang. Kalo berjalan hap-hap-hap.")
